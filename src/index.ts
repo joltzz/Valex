a29231d5caffc55c132a53ef7f2f7d6929c02965
joltzz <guijsilva02@gmail.com>
2022-09-05 16:40:08 -0300

    feat: middlewares added.

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import 'express-async-errors';
-import router from "./routes/index.ts"
+import router from "./routes/index"
+import errorHandler from "./middlewares/errorHandler";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
-
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000
 
